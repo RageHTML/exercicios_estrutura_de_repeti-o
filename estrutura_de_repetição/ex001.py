629942c24b082d199760feb8fd16f91560f9497d
Deyvid <deyvidm24@gmail.com>
2025-04-09 12:11:06 -0300

    readme.md

--- estrutura_de_repetição/ex001.py
+++ estrutura_de_repetição/ex001.py
@@ -1,8 +1,9 @@
 for x in range(0,10):
     x = x + 1
     numero = int(input(f"Usuário {x}, insira o número: ")) 
-    print(f"O triplo de {numero} = {triplo}")
     triplo = numero * 3 
+    print(f"O triplo de {numero} = {triplo}")
+
     if numero >= 0:
         print("positivo")
     if numero <= 0: 
