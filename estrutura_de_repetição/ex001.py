7607cba1ee6722ebc7530149451684a7c36d0ad8
Deyvid <deyvidm24@gmail.com>
2025-04-09 11:54:44 -0300

    readm.md

--- estrutura_de_repetição/ex001.py
+++ estrutura_de_repetição/ex001.py
@@ -1,13 +1,9 @@
 for x in range(0,10):
-    x = x + 1 # identificador do loop
-    numero = int(input(f"Usuário {x}, insira o número: ")) # input do número
-    triplo = numero * 3 # calculando o triplos
+    x = x + 1
+    numero = int(input(f"Usuário {x}, insira o número: ")) 
     print(f"O triplo de {numero} = {triplo}")
+    triplo = numero * 3 
     if numero >= 0:
         print("positivo")
     if numero <= 0: 
         print("negativo") 
-        
-
-# O programa ler 10 numeros e retorna se o número é positivo e o seu triplo
-    
